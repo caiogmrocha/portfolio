95c8d4b51af6dfa39f595df1e152813d296b0099
caiogmrocha <caioguilherme739@gmail.com>
2024-11-09 17:22:11 -0300

    refactor(#5): improve <SocialsSection /> organization

--- src/pages/components/SocialsSection.tsx
+++ src/pages/components/SocialsSection.tsx
@@ -6,49 +6,47 @@ import { SocialArticle } from "./SocialArticle";
 
 
 export function SocialsSection() {
-    const instagramUrl = "https://www.instagram.com/caio.guir/";
-    const twitterUrl = "https://x.com/CaioGuir";
-    const linkedinUrl = "https://www.linkedin.com/in/caiogmrocha/";
-    const githubUrl = "https://github.com/caiogmrocha/";
+    const sectionsData = [
+        {
+            title: "LinkedIn",
+            description: "Caio Rocha",
+            url: "https://www.linkedin.com/in/caiogmrocha/",
+            icon: LinkedinIcon,
+        },
+        {
+            title: "Github",
+            description: "caio.gm.rocha",
+            url: "https://github.com/caiogmrocha/",
+            icon: GithubIcon,
+        },
+        {
+            title: "Instagram",
+            description: "@caio.guir",
+            url: "https://www.instagram.com/caio.guir/",
+            icon: InstagramIcon,
+        },
+        {
+            title: "Twitter",
+            description: "@CaioGuir",
+            url: "https://x.com/CaioGuir",
+            icon: TwitterIcon,
+        },
+    ]
 
     return (
         <section className="flex flex-col gap-5">
-            <SocialArticle
-                icon={<img src={LinkedinIcon} alt="Linkedin" className="h-[48px] w-[48px]" />}
-                title="LinkedIn"
-                description={
-                    <a href={linkedinUrl} target="_blank" rel="noreferrer" className="hover:underline">
-                        Caio Rocha
-                    </a>
-                }
-            />
-            <SocialArticle
-                icon={<img src={GithubIcon} alt="Github" className="h-[48px] w-[48px]" />}
-                title="Github"
-                description={
-                    <a href={githubUrl} target="_blank" rel="noreferrer" className="hover:underline">
-                        caiogmrocha
-                    </a>
-                }
-            />
-            <SocialArticle
-                icon={<img src={InstagramIcon} alt="Instagram" className="h-[48px] w-[48px]" />}
-                title="Instagram"
-                description={
-                    <a href={instagramUrl} target="_blank" rel="noreferrer" className="hover:underline">
-                        @caio.guir
-                    </a>
-                }
-            />
-            <SocialArticle
-                icon={<img src={TwitterIcon} alt="Twitter" className="h-[48px] w-[48px]" />}
-                title="Twitter"
-                description={
-                    <a href={twitterUrl} target="_blank" rel="noreferrer" className="hover:underline">
-                        @CaioGuir
-                    </a>
-                }
-            />
+            {sectionsData.map((section) => (
+                <SocialArticle
+                    key={section.title}
+                    icon={<img src={section.icon} alt="Linkedin" className="h-[48px] w-[48px]" />}
+                    title={section.title}
+                    description={
+                        <a href={section.url} target="_blank" rel="noreferrer" className="hover:underline">
+                            {section.description}
+                        </a>
+                    }
+                />
+            ))}
         </section>
     );
 }

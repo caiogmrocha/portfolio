dfbda9d0c3180b71e865b6683d419e5d2158e257
caiogmrocha <caioguilherme739@gmail.com>
2024-11-08 23:33:22 -0300

    feat(#2): create PresentationSection

---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,10 @@
+import { PresentationSection } from "./components/PresentationSection";
+
+export function PortfolioPage() {
+    return (
+        <div className="container mx-auto flex">
+            <PresentationSection/>
+
+        </div>
+    );
+}
---
+++ src/pages/components/PresentationSection.tsx
@@ -0,0 +1,17 @@
+import ProfilePicture from "../../assets/images/profile-picture.jpg";
+
+type PresentationSectionProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function PresentationSection(props: PresentationSectionProps) {
+    return (
+        <section {...props}>
+            <img src={ProfilePicture} alt="Profile Picture" className="h-24 w-h-24 rounded-full" />
+            <h1 className="text-3xl font-bold mt-2">
+                Caio Rocha
+            </h1>
+            <h2 className="text-2xl font-bold bg-gradient-to-r from-indigo-500 vi-blue-600 to-cyan-400 text-transparent bg-clip-text inline-block mt-1">
+                Software Developer
+            </h2>
+        </section>
+    );
+}

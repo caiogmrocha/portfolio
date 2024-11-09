625042c54a6392a01db9b26f8cfa479c4afd79b7
caiogmrocha <caioguilherme739@gmail.com>
2024-11-09 17:24:39 -0300

    refactor(#5): improve <ContactsSection /> organization

--- src/pages/components/ContactsSection.tsx
+++ src/pages/components/ContactsSection.tsx
@@ -5,37 +5,47 @@ export function ContactsSection() {
     const email = "caioguilherme739@gmail.com";
     const phone = "+55 87 98144-3773";
 
+    const sectionsData = [
+        {
+            title: "E-mail",
+            description: (
+                <a href={`mailto:${email}`} className="hover:underline">
+                    {email}
+                </a>
+            ),
+            icon: MailIcon,
+        },
+        {
+            title: "Phone",
+            description: (
+                <a href={`tel:${phone}`} className="hover:underline">
+                    {phone}
+                </a>
+            ),
+            icon: PhoneIcon,
+        },
+        {
+            title: "Endereço",
+            description: (
+                <address className="not-italic">
+                    <a href="https://www.google.com/maps/place/Garanhuns,+PE" target="_blank" rel="noreferrer" className="hover:underline">
+                        Garanhuns, Pernambuco, Brazil
+                    </a>
+                </address>
+            ),
+            icon: MapPinIcon,
+        },
+    ]
+
     return (
         <section className="flex flex-col gap-5">
-            <ContactArticle
-                icon={<MailIcon className="h-5" />}
-                title="E-mail"
-                description={
-                    <a href={`mailto:${email}`} className="hover:underline">
-                        {email}
-                    </a>
-                }
-            />
-            <ContactArticle
-                icon={<PhoneIcon className="h-5" />}
-                title="Phone"
-                description={
-                    <a href={`tel:${phone}`} className="hover:underline">
-                        {phone}
-                    </a>
-                }
-            />
-            <ContactArticle
-                icon={<MapPinIcon className="h-5" />}
-                title="Endereço"
-                description={
-                    <address className="not-italic">
-                        <a href="https://www.google.com/maps/place/Garanhuns,+PE" target="_blank" rel="noreferrer" className="hover:underline">
-                            Garanhuns, Pernambuco, Brazil
-                        </a>
-                    </address>
-                }
-            />
+            {sectionsData.map((section) => (
+                <ContactArticle
+                    icon={<section.icon />}
+                    title={section.title}
+                    description={section.description}
+                />
+            ))}
         </section>
     );
 }

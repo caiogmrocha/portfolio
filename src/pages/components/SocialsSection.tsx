df7181a8a618d1235f702d08e683b5825554f230
caiogmrocha <caioguilherme739@gmail.com>
2024-11-09 20:25:43 -0300

    refactor(#5): improve <SocialArticle /> and <SocialsSection> organization

--- src/pages/components/SocialsSection.tsx
+++ src/pages/components/SocialsSection.tsx
@@ -2,47 +2,51 @@ import LinkedinIcon from '../../assets/images/socials/linkedin-icon.svg';
 import GithubIcon from '../../assets/images/socials/github-icon.svg';
 import InstagramIcon from '../../assets/images/socials/instagram-icon.svg';
 import TwitterIcon from '../../assets/images/socials/twitter-icon.svg';
-import { SocialArticle } from "./SocialArticle";
+import { SocialArticle, SocialArticleProps } from "./SocialArticle";
 
+type AdaptedSocialArticleProps = Pick<SocialArticleProps, 'title' | 'description'> & {
+    iconPath: string
+    url: string
+};
 
 export function SocialsSection() {
-    const sectionsData = [
+    const socialsArticlesProps: AdaptedSocialArticleProps[] = [
         {
             title: "LinkedIn",
             description: "Caio Rocha",
             url: "https://www.linkedin.com/in/caiogmrocha/",
-            icon: LinkedinIcon,
+            iconPath: LinkedinIcon,
         },
         {
             title: "Github",
-            description: "caio.gm.rocha",
+            description: "caiogmrocha",
             url: "https://github.com/caiogmrocha/",
-            icon: GithubIcon,
+            iconPath: GithubIcon,
         },
         {
             title: "Instagram",
             description: "@caio.guir",
             url: "https://www.instagram.com/caio.guir/",
-            icon: InstagramIcon,
+            iconPath: InstagramIcon,
         },
         {
             title: "Twitter",
             description: "@CaioGuir",
             url: "https://x.com/CaioGuir",
-            icon: TwitterIcon,
+            iconPath: TwitterIcon,
         },
-    ]
+    ];
 
     return (
         <section className="flex flex-col gap-5">
-            {sectionsData.map((section) => (
+            {socialsArticlesProps.map((socialArticleProps) => (
                 <SocialArticle
-                    key={section.title}
-                    icon={<img src={section.icon} alt="Linkedin" className="h-[48px] w-[48px]" />}
-                    title={section.title}
+                    key={socialArticleProps.title}
+                    icon={<img src={socialArticleProps.iconPath} alt="Linkedin" className="h-[48px] w-[48px]" />}
+                    title={socialArticleProps.title}
                     description={
-                        <a href={section.url} target="_blank" rel="noreferrer" className="hover:underline">
-                            {section.description}
+                        <a href={socialArticleProps.url} target="_blank" rel="noreferrer" className="hover:underline">
+                            {socialArticleProps.description}
                         </a>
                     }
                 />
